29c29d36e2bd6e385937f9bece147a3a0f76d2b5
DOYOUNG-0314 <a01040907674@gmail.com>
2025-03-14 18:57:10 +0900

    코어타임전

--- 10) x보다.py
+++ 10) x보다.py
@@ -1,5 +1,7 @@
 N, X = map(int,input().split()) 
 (1 <= N, X <= 10,000)
 
+for _ in range(N):
+    
 
 
---
+++ 13) 문자열반복.py
@@ -0,0 +1,13 @@
+T = int(input())
+(1<=T<=1000)
+for i in range(T):
+    R,S = input().split(maxsplit=1)
+    R=int(R)
+    for j in S:
+        print(R*j)
+       
+
+
+
+    
+
