d43efbc2d021f80b482ba671edf175719cf747bd
DOYOUNG-0314 <a01040907674@gmail.com>
2025-03-26 02:27:51 +0900

    크게 만들기

---
+++ week02/22) 크게 만들기.py
@@ -0,0 +1,18 @@
+N, K=map(int,input().split())
+number = input().strip()
+stack=[]
+count = 0
+for digit in number:
+    while stack and count < K and stack[-1]<digit:
+        stack.pop()
+        count += 1
+    stack.append(digit)
+
+while count < K:
+    stack.pop()
+    count += 1
+
+print(*stack)
+
+
+    
